7a41bac7f2b722d393aa8c655115eb80a68ac968
依云 <lilydjwg@gmail.com>
2016-07-15 18:14:13 +0800

    multi threading example

--- examples/packet_handler.rs
+++ examples/packet_handler.rs
@@ -1,21 +1,35 @@
 extern crate netfilter_queue as nfq;
+extern crate threadpool;
 
 use std::ptr::null;
 use nfq::handle::{Handle, ProtocolFamily};
 use nfq::queue::{CopyMode, Verdict, PacketHandler, QueueHandle};
-use nfq::message::Message;
+use nfq::message::{Message, IPHeader};
 use nfq::error::Error;
+use threadpool::ThreadPool;
+use std::sync::mpsc::channel;
+use std::sync::Arc;
 
 fn main() {
     let mut handle = Handle::new().ok().unwrap();
     handle.bind(ProtocolFamily::INET).ok().unwrap();
 
-    let mut queue = handle.queue(0, Decider).ok().unwrap();
-    let _ = queue.set_mode(CopyMode::Metadata).unwrap();
+    let mut queue = handle.queue(1, Decider).ok().unwrap();
+    queue.set_mode_sized::<IPHeader>().ok().unwrap();
 
     println!("Listening for packets...");
-    handle.start(4096);
-
+    let pool = ThreadPool::new(2);
+    let (tx, rx) = channel();
+    let arc = Arc::new(handle);
+    for _ in 0..2 {
+      let tx = tx.clone();
+      let arc = arc.clone();
+      pool.execute(move || {
+        arc.start(4096).unwrap();
+        tx.send(()).unwrap();
+      });
+    }
+    let _: Vec<_> = rx.iter().take(2).collect();
     println!("...finished.");
 }
 
@@ -26,6 +40,8 @@ impl PacketHandler for Decider {
     fn handle(&mut self, hq: QueueHandle, message: Result<&Message, &Error>) -> i32 {
         match message {
             Ok(m) => {
+                let h = unsafe { m.ip_header() }.unwrap();
+                println!("Packet received: {:?} -> {:?}", h.saddr(), h.daddr());
                 let _ = Verdict::set_verdict(hq, m.header.id(), Verdict::Accept, 0, null());
             },
             Err(_) => ()
